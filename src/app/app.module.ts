4663f4e25763aa4b2a43bce3d64328f06d69ec8e
mohdShahwan <mshahwan002@gmail.com>
2022-12-16 17:30:10 +0300

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,33 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBluRE_4K2MpRsnCRAitpo7jvHP8rKE28o",
+  authDomain: "cold-store-app.firebaseapp.com",
+  projectId: "cold-store-app",
+  storageBucket: "cold-store-app.appspot.com",
+  messagingSenderId: "658910761829",
+  appId: "1:658910761829:web:d7d31b624f80cacf2b078c",
+  measurementId: "G-CFNLW4SJYZ"
+};
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
+
+
+
