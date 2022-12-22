a0a46495b1659d0b1436c32aeb36ecdcdf54afba
mohdShahwan <mshahwan002@gmail.com>
2022-12-23 02:25:47 +0300

    Register

--- src/app/fb.service.ts
+++ src/app/fb.service.ts
@@ -4,8 +4,8 @@ import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { DocumentReference } from '@angular/fire/compat/firestore';
 import { map, take } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { AlertController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastController } from '@ionic/angular';
 
 export interface User {
   id?: string,
@@ -20,10 +20,10 @@ export interface User {
 })
 
 export class FbService {
-  private users: Observable<User[]>;
-  private usersCollection: AngularFirestoreCollection<User>;
+  public users: Observable<User[]>;
+  public usersCollection: AngularFirestoreCollection<User>;
 
-  constructor(private  afs:  AngularFirestore, private  afAuth: AngularFireAuth) { 
+  constructor(private  afs:  AngularFirestore, private  afAuth: AngularFireAuth, private toastCtrl: ToastController) { 
     this.usersCollection = this.afs.collection<User>('users');
     this.users = this.usersCollection.snapshotChanges().pipe(
       map(actions => {
@@ -36,12 +36,35 @@ export class FbService {
     );
   }
 
-  addUser(user:  User):  Promise<DocumentReference>  {
-    return  this.usersCollection.add(user); 
-}
+  async showToast(message: string, color: string){
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: "bottom",
+      color: color,
+    });
+    await toast.present();
+  }
 
-  SignUp(newUser:  User, newEmail: string, newPassword: string): Promise<any> {
-    this.addUser(newUser);
+  addUser(user:  User):  Promise<DocumentReference>  {
+    return this.usersCollection.add(user);
+  }
+
+  getUsers():  Observable<User[]>  {
+    return  this.users;
+  }
+  
+  getUser(id: string): Observable<User | undefined>{
+    return this.usersCollection.doc<User>(id).valueChanges().pipe(
+      map(user => {
+        if(user)
+          user.id = id;
+        return user
+      })
+    );
+  }
+
+  SignUp(newEmail: string, newPassword: string): Promise<any> {
     return this.afAuth.createUserWithEmailAndPassword(newEmail, newPassword);
   }
 
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FbService } from '../fb.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private fb: FbService) {}
+
+  fName: string = '';
+  lName: string = '';
+  email: string = '';
+  phone: string = '';
+  password1: string = '';
+  password2: string = '';
+  userType: string = '';
+  
+  registerUser(){
+    if(this.password1 == this.password2){
+      this.fb.SignUp(this.email, this.password1).then(res => {
+        this.fb.addUser({
+          name: this.fName + ' ' + this.lName,
+          email: this.email,
+          phone: this.phone,
+          userType: this.userType
+        });
+        this.fb.showToast('User created successfully', 'success');
+      }).catch(err => {
+        if(err.code == 'auth/email-already-in-use')
+          this.fb.showToast('Email already in use', 'danger');
+        else if(err.code == 'auth/invalid-email')
+          this.fb.showToast('Invalid email', 'danger');
+        else if(err.code == 'auth/weak-password')
+          this.fb.showToast('Password too weak', 'danger');
+        else
+          this.fb.showToast(('Error: ' + err.code), 'danger');
+      });
+    }
+  }
+
 
 }
