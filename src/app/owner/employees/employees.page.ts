cc23dc0663dc47abcf31be4bb0e100a00b0f5bb9
mohdShahwan <mshahwan002@gmail.com>
2022-12-25 19:19:16 +0300

    View schedule of an employee

--- src/app/owner/employees/employees.page.ts
+++ src/app/owner/employees/employees.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FbService, Slot, User } from 'src/app/fb.service';
 
 @Component({
   selector: 'app-employees',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public fb: FbService,
+  ) { }
 
   ngOnInit() {
   }
 
+  selectedEmp: User = {} as User;
+  selectedEmpSlots: Slot[] = [];
+  empModal: boolean = false;
+  showEmpSch(emp: User){
+    this.empModal = true;
+    // Set selected employee
+    this.selectedEmp = emp;
+    // Get employee's schedule slots
+    this.selectedEmpSlots = this.fb.allSlots.filter(slot => slot.employee.id == emp.id);
+    // Sort slots by date
+    this.selectedEmpSlots.sort((a, b) => {
+      return new Date(a.date).getTime() - new Date(b.date).getTime();
+    });
+    // Show employee's schedule slots
+  }
 }
