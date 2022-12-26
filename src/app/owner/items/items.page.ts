b485982d7cf36024a40e037fd0f749d9df1a8759
mohdShahwan <mshahwan002@gmail.com>
2022-12-26 16:19:05 +0300

    Threshold update by owner

--- src/app/owner/items/items.page.ts
+++ src/app/owner/items/items.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FbService } from 'src/app/fb.service';
+import { FbService, StoreItem } from 'src/app/fb.service';
 
 @Component({
   selector: 'app-items',
@@ -15,4 +15,20 @@ export class ItemsPage implements OnInit {
   ngOnInit() {
   }
 
+  editStoreItem(item: StoreItem) {
+    //this.fb.editStoreItem(item);
+  }
+  
+  selectedItems: StoreItem[] = [];
+  checkItem(sItem: StoreItem) {
+    return this.selectedItems.filter(item => sItem == item).length > 0;
+  }
+  toggleItem(sItem: StoreItem) {
+    if (this.checkItem(sItem)) {
+      this.selectedItems = this.selectedItems.filter(item => item != sItem);
+    } else {
+      this.selectedItems.push(sItem);
+    }
+    console.log(this.selectedItems);
+  }
 }
